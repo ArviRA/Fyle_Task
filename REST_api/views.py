e616bbb5238ff269f6c05782aed972dd6ed0f0ef
ArviRA <arvindhak47@gmail.com>
2021-06-25 16:24:42 +0530

    started

--- REST_api/views.py
+++ REST_api/views.py
@@ -10,9 +10,9 @@ def autoComplete(request):
     if request.method == 'GET':
         response = request.GET.dict()
         print("resu:::",response['q'])
-        return JsonResponse({'status':'200'})
+        return JsonResponse({'Status Code':'200'})
     else:
-        return JsonResponse({"Status Code":'405'})
+        return JsonResponse({"Status Code":'405','Message':"Wrong Method"})
 
 def empty(request):
-    return JsonResponse({'empty':'404'})
+    return JsonResponse({'Status Code':'404','Message':"Page Not Found"})

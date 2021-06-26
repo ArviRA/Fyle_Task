d942606ef4f58daf8b6ba299119b536e60272ca9
ArviRA <arvindhak47@gmail.com>
2021-06-26 20:41:03 +0530

    search query completed

--- REST_api/urls.py
+++ REST_api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('branches/autocomplete',views.autoComplete,name=""),
+    path('branches/search',views.search,name=""),
     path("",views.empty,name=""),
 ]
--- REST_api/views.py
+++ REST_api/views.py
@@ -21,7 +21,25 @@ def autoComplete(request):
         query = response['q'].upper()
         limit = response['limit']
         offset = response['offset']
-        code = "select * from bank where branch like '%{}%' limit {} offset {}".format(query,limit,offset)
+        code = "select * from bank where branch like '%{}%' order by ifsc limit {} offset {}".format(query,limit,offset)
+        final_res = getRecord(cur,code)
+        return JsonResponse({'Status Code':'200','result':final_res})
+    else:
+        return JsonResponse({"Status Code":'405','Message':"Wrong Method"})
+def search(request):
+    if request.method == "GET":
+        global cur
+        if cur == None:
+            cur = getConnection()
+            if cur['status'] == 200:
+                cur = cur['data']
+            else:
+                return JsonResponse(cur)
+        response = request.GET.dict()
+        query = response['q'].upper()
+        limit = response['limit']
+        offset = response['offset']
+        code = "select * from bank where ifsc like '%{0}%' or bank_id like '%{0}%' or branch like '%{0}%' or address like '%{0}%' or city like '%{0}%' or district like '%{0}%' or state like '%{0}%' or bank_name like '%{0}%'  order by ifsc limit {1} offset {2} ".format(query,limit,offset)
         final_res = getRecord(cur,code)
         return JsonResponse({'Status Code':'200','result':final_res})
     else:
